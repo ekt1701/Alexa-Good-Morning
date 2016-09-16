386786e465607e297534227813a252ec14e1582d
ekt1701 <ekt1701@gmail.com>
2016-09-15 19:34:48 -0700

    Add files via upload

--- good_morning.py
+++ good_morning.py
@@ -228,7 +228,21 @@ def reportTraffic():
     response = urllib2.urlopen(url)
     data = dict(json.loads(response.read()))
     duration = data['routes'][0]['legs'][0]['duration']['text']
-    reportTraffic = "Average traffic time to your destination is " +str(duration)
+    route = data['routes'][0]['legs'][0]['steps']
+    direct = []
+    for step in route:
+        directions = step['html_instructions']
+        direct.append(directions)
+    instructions = ', '.join(direct)
+
+    temp = "<body"+str(instructions)+"</body"
+    text = html2text(temp)
+
+    replace = {
+            "Dest" : " Dest"
+            }
+    text = multiple_replace(replace, text)
+    reportTraffic = "Average traffic time to your destination is " +str(duration) + " Here are the Directions: Head "+str(text)
     return reportTraffic
 
 
@@ -699,6 +713,47 @@ def multiple_replace(dict, text):
 
     # For each match, look-up corresponding value in dictionary
     return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
+
+def html2text(strText):
+    str1 = strText
+    int2 = str1.lower().find("<body")
+    if int2>0:
+       str1 = str1[int2:]
+    int2 = str1.lower().find("</body>")
+    if int2>0:
+       str1 = str1[:int2]
+    list1 = ['<br>',  '<tr',  '<td', '</p>', 'span>', 'li>', '</h', 'div>', '<em>', '</em>']
+    list2 = [chr(13), chr(13), chr(9), chr(13), chr(13),  chr(13), chr(13), chr(13), chr(13), chr(13)]
+    bolFlag1 = True
+    bolFlag2 = True
+    strReturn = ""
+    for int1 in range(len(str1)):
+      str2 = str1[int1]
+      for int2 in range(len(list1)):
+        if str1[int1:int1+len(list1[int2])].lower() == list1[int2]:
+           strReturn = strReturn + list2[int2]
+      if str1[int1:int1+7].lower() == '<script' or str1[int1:int1+9].lower() == '<noscript':
+         bolFlag1 = False
+      if str1[int1:int1+6].lower() == '<style':
+         bolFlag1 = False
+      if str1[int1:int1+7].lower() == '</style':
+         bolFlag1 = True
+      if str1[int1:int1+9].lower() == '</script>' or str1[int1:int1+11].lower() == '</noscript>':
+         bolFlag1 = True
+      if str2 == '<':
+         bolFlag2 = False
+      if bolFlag1 and bolFlag2 and (ord(str2) != 10) :
+        strReturn = strReturn + str2
+      if str2 == '>':
+         bolFlag2 = True
+      if bolFlag1 and bolFlag2:
+        strReturn = strReturn.replace(chr(32)+chr(13), chr(13))
+        strReturn = strReturn.replace(chr(9)+chr(13), chr(13))
+        strReturn = strReturn.replace(chr(13)+chr(32), chr(13))
+        strReturn = strReturn.replace(chr(13)+chr(9), chr(13))
+        strReturn = strReturn.replace(chr(13)+chr(13), chr(13))
+    strReturn = strReturn.replace(chr(13), chr(32))
+    return strReturn
 
 # --------------- Helpers that build all of the responses ----------------------
 
